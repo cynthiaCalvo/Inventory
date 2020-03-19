319a59821edc937a5daf40bf51f648071e7a887f
cynthia <calvocyn@gmail.com>
2020-03-19 09:23:06 -0500

    comment

--- arrayList2/src/arrayList2/Inventory.java
+++ arrayList2/src/arrayList2/Inventory.java
@@ -8,29 +8,54 @@ public class Inventory {
 	private String itemName;
 	private int stockAmount, stockNum;
 	
+	/**
+	 * will set the class variables from what the user entered
+	 * @param name
+	 * @param amount
+	 * @param num
+	 */
 	public Inventory(String name, int amount, int num) {
 		itemName = name;
 		stockAmount = amount;
 		stockNum = num;
 	}
 	
+	/**
+	 * can be called to discontinue an item
+	 */
 	public void discontinueItem() {
 		itemName = "discontinued";
 		stockAmount = 0;
 	}
 	
+	/**
+	 * can be called to change the amount of an item in stock
+	 * @param amount
+	 */
 	public void changeStockAmount(int amount) {
-		stockAmount = amount;
+		stockAmount += amount;
 	}
 	
+	/**
+	 * will return the amount of stock for a specific item
+	 * @return
+	 */
 	public int returnStockAmount() {
 		return stockAmount;
 	}
 	
+	/**
+	 * will return the stock number of an item
+	 * @return
+	 */
 	public int returnStockNum() {
 		return stockNum;
 	}
 	
+	/**
+	 * will return the name of the item
+	 * @return
+	 */
 	public String returnItemName() {
 		return itemName;
 	}
--- arrayList2/src/arrayList2/Main.java
+++ arrayList2/src/arrayList2/Main.java
@@ -19,14 +19,22 @@ public class Main {
 		
 		ArrayList<Inventory> obj = new ArrayList<Inventory>();	
 		
+		//will loop until user chooses to exit(5)
 		do {
 			y = 0;
-			System.out.println("Choose from the following options: ");
+			System.out.println("\nChoose from the following options: ");
 			System.out.println("1.add new item\n2.discontinue item\n3.display amount in stock for item\n4.add stock to item\n5.exit");
 			System.out.print("\nEnter your choice: ");
 			userChoice = inputInt.nextInt();
+			
+			//loops until user enters a valid choice
+			while(userChoice > 5 || userChoice < 1) {
+				System.out.print("Please enter a valid option: ");
+				userChoice = inputInt.nextInt();
+			}
 			System.out.println();
 			
+			//will choose which action depending on userChoice
 			if(userChoice == 1) {
 				System.out.print("What is the name of the item? ");
 				itemName = inputString.nextLine();
@@ -35,34 +43,43 @@ public class Main {
 				stockAmount = inputInt.nextInt();
 				
 				obj.add(new Inventory(itemName, stockAmount, stockNum));
-				//print out how many were added & item #
+				System.out.println(stockAmount + " " + itemName + " have been added");
+				System.out.println(itemName + "'s item number is: " + stockNum);
 				stockNum+=1;
 			}else if(userChoice > 1 && userChoice < 5) {
 				System.out.print("What is the item number? ");
 				getStockNum[0] = inputInt.nextInt();
 				
+				//will loop until the stockNum and the stock number the user entered match
 				do{
-					y++;
 					getStockNum[1] = obj.get(y).returnStockNum();
+					y++;
+					//will inturupt the loop if the user has entered a value that does not exist in any part of the array
+					if(y >= obj.size()) {
+						System.out.print("Please enter a valid item number: ");
+						getStockNum[0] = inputInt.nextInt();
+						y = 0;
+					}
 				}while(getStockNum[0] != getStockNum[1]);
 				
 			}else {
 				System.out.print("\nBye-Bye");
 			}
 			
+			//will finish the actions depending on userChoice
 			if(userChoice == 2) {
-				System.out.println(obj.get(y).returnItemName() + " has been discontinued");
-				//how to get discontinuedItem method????
+				System.out.println(obj.get(getStockNum[0] - 1000).returnItemName() + " has been discontinued");
+				obj.get(getStockNum[0] - 1000).discontinueItem();
 			}else if(userChoice == 3) {
-				System.out.println("There are " + obj.get(y).returnStockAmount() + " " + obj.get(y).returnItemName());
+				System.out.println("There are " + obj.get(getStockNum[0] - 1000).returnStockAmount() + " " + obj.get(getStockNum[0] - 1000).returnItemName());
 			}else if(userChoice == 4) {
-				
+				System.out.print("How many are you adding? ");
+				obj.get(getStockNum[0] - 1000).changeStockAmount(inputInt.nextInt());
+				System.out.println("There are " + obj.get(getStockNum[0] - 1000).returnStockAmount() + " " + obj.get(getStockNum[0] - 1000).returnItemName());
 			}else {
 				System.out.print("");
 			}
 			
 		}while(userChoice != 5);
-	
 	}
-
 }
